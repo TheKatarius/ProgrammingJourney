cf53c1e22ed19b129c7c878ef6467bd4f0bd0efd
TheKatarius <hubert558@interia.pl>
2023-10-11 12:57:36 +0200

    Article Preview Component - Changing Share Icon Color Fixed

--- FrontendMentorProjects/Article Preview Component/article-preview-component-master/main.js
+++ FrontendMentorProjects/Article Preview Component/article-preview-component-master/main.js
@@ -1,12 +1,20 @@
 const shareIconFigure = document.querySelector(".share-icon-figure");
+const shareIconWhite = document.querySelector(".share-icon-white");
+const shareIconBlack = document.querySelector(".share-icon-black");
 const shareWindowFigure = document.querySelector(".share-window-figure");
 
 let isClicked = false;
-shareIconFigure.addEventListener("click", (event) => {
+shareIconFigure.addEventListener("click", () => {
 	if (!isClicked) {
 		shareWindowFigure.style.display = "flex";
+		shareIconFigure.style.backgroundColor = "hsl(214, 17%, 51%)";
+		shareIconBlack.style.display = "none";
+		shareIconWhite.style.display = "block";
 	} else {
 		shareWindowFigure.style.display = "none";
+		shareIconFigure.style.backgroundColor = "white";
+		shareIconBlack.style.display = "block";
+		shareIconWhite.style.display = "none";
 	}
 
 	isClicked = !isClicked;
