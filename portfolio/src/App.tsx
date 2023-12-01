2c138579f5dda963a6efa21959415dc1c79de1e9
TheKatarius <hubert558@interia.pl>
2023-12-01 21:42:00 +0100

    Portfolio - Part I

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,5 @@
+import { Header } from "./components/Header/Header";
+
+export function App() {
+	return <Header />;
+}
---
+++ portfolio/src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+export function Header() {
+	return (
+		<header>
+			<h3>{`< Hubert Łazaj />`}</h3>
+			<ul>
+				<li>
+					<a href="">Home</a>
+				</li>
+				<li>
+					<a href="">About</a>
+				</li>
+				<li>
+					<a href="">Projects</a>
+				</li>
+				<li>
+					<a href="">Contact</a>
+				</li>
+				<li>
+					<a href="">Resume</a>
+				</li>
+			</ul>
+		</header>
+	);
+}
